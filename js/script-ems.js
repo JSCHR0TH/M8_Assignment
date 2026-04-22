b7729e34dd33fd07404416f30c5d7fedd5d7428f
Jana Schroth <schroth.jana@gmail.com>
2026-04-21 23:21:31 -0700

    ems complete

--- js/script-ems.js
+++ js/script-ems.js
@@ -8,15 +8,15 @@ let employees = [
 ]
 
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
-// IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
 if (localStorage.getItem('employees')) {
+    // IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
     employees = JSON.parse(localStorage.getItem('employees'))
 }
 
 // GET DOM ELEMENTS
 let form     = document.querySelector('#addForm');
 let empTable    = document.querySelector('#empTable');
-let tBody = table.querySelector('tbody')
+let tbody = empTable.getElementsByTagName('tbody')[0];
 let empCount    = document.querySelector('#empCount');
 
 // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
@@ -26,41 +26,54 @@ buildGrid();
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
     e.preventDefault();
+
     // GET THE VALUES FROM THE TEXT BOXES
     let id          = document.getElementById('id').value;
     let name        = document.getElementById('name').value;
     let ext         = document.getElementById('extension').value;
     let email       = document.getElementById('email').value;
     let department  = document.getElementById('department').value;
+
     // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
-    let newEmployee = [id, name, ext, email, department]
+    let newEmployee = [id, name, ext, email, department];
+
     // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
     employees.push(newEmployee);
+
     // BUILD THE GRID
     buildGrid();
 
     // RESET THE FORM
     form.reset();
+
     // SET FOCUS BACK TO THE ID TEXT BOX
-    document.getElementById('id').focus();
+    document.querySelector('#addForm').focus();
 });
 
 // DELETE EMPLOYEE
 empTable.addEventListener('click', (e) => {
     // CONFIRM THE DELETE
+    if(e.target.tagName === 'BUTTON') {
+        let confirmDelete = confirm ('Are you sure you want to delete this employee?');
 
-        // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
+        if (confirmDelete) {
+            // GET THE SELECTED ROWINDEX FOR THE TR (PARENTNODE.PARENTNODE)
+            let row = e.target.parentNode.parentNode;
+            let index = row.rowIndex - 1;
 
-        // REMOVE EMPLOYEE FROM ARRAY
+            // REMOVE EMPLOYEE FROM ARRAY
+            employees.splice(index, 1);
 
-        // BUILD THE GRID
-        buildGrid();
-});
+            // BUILD THE GRID
+            buildGrid();
+        }
+    }
+})
 
 // BUILD THE EMPLOYEES GRID
 function buildGrid() {
     // REMOVE THE EXISTING SET OF ROWS BY REMOVING THE ENTIRE TBODY SECTION
-    tBody.innerHTML = ''
+    tbody.innerHTML = '';
     // REBUILD THE TBODY FROM SCRATCH
 
     // LOOP THROUGH THE ARRAY OF EMPLOYEES
@@ -75,11 +88,11 @@ function buildGrid() {
             <td>${employee [2]}</td>
             <td>${employee [3]}</td>
             <td>${employee [4]}</td>
-            <td><button class = "btn btn-danger btn-sm">X</button>/button></td> `
+            <td><button class = "btn btn-danger btn-sm">X</button></td> `;
     
 
         // BIND THE TBODY TO THE EMPLOYEE TABLE
-        tBody.appendChild(row)
+        tbody.appendChild(row)
     }
 
     // UPDATE EMPLOYEE COUNT
