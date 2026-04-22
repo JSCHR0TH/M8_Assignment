3bae9243a157ed7eb7ccf3423548da8775993680
Jana Schroth <schroth.jana@gmail.com>
2026-04-21 20:38:24 -0700

    initial commit

---
+++ js/script-array-practice.js
@@ -0,0 +1,91 @@
+
+
+
+/*
+//STEP 1
+let movies = ['The Intouchables', 'The Last Samurai', 'Pulp Fiction', 'The Prestige', 'Gladiator']
+console.log(movies[1])
+
+//STEP 2
+let movies = []
+movies [0] = 'The Intouchables'
+movies [1] = 'The Last Samurai'
+movies [2] = 'Pulp Fiction'
+movies [3] = 'The Prestige'
+movies [4] = 'Gladiator'
+
+console.log(movies)
+
+//STEP 3
+let movies = []
+movies [0] = 'The Intouchables'
+movies [1] = 'The Last Samurai'
+movies [2] = 'Pulp Fiction'
+movies [3] = 'The Prestige'
+movies [4] = 'Gladiator'
+
+movies [5] = 'Minority Report'
+
+console.log(movies)
+
+
+//STEP 4
+let movies = ['The Intouchables', 'The Last Samurai', 'Pulp Fiction', 'The Prestige', 'Gladiator']
+delete movies [0]
+console.log(movies)
+
+
+//STEP 5
+let movies = ['The Intouchables', 'The Last Samurai', 'Pulp Fiction', 'The Prestige', 'Gladiator', 'Minority Report', 'The Godfather']
+for (movie in movies) {
+    console.log(movies[movie])
+}
+
+
+//STEP 6
+let movies = ['The Intouchables', 'The Last Samurai', 'Pulp Fiction', 'The Prestige', 'Gladiator', 'Minority Report', 'The Godfather']
+for (movie of movies) {
+    console.log(movie)
+}
+
+
+//STEP 7
+let movies = ['The Intouchables', 'The Last Samurai', 'Pulp Fiction', 'The Prestige', 'Gladiator', 'Minority Report', 'The Godfather']
+movies.sort()
+for (movie of movies) {
+    console.log(movie)
+}
+*/
+
+//STEP 8
+let movies = ['The Intouchables', 'The Last Samurai', 'Pulp Fiction', 'The Prestige', 'Gladiator', 'Minority Report', 'The Godfather']
+let leastFavMovies = ['Bladerunner', 'I know what you did last summer', 'South Park']
+
+console.log('Movies I like: \n')
+for (movie of movies) {
+    console.log(movie)
+}
+
+console.log('Movies I regret watching: \n')
+for (movie of leastFavMovies) {
+    console.log(movie)
+}
+
+
+//STEP 9
+
+//STEP 10
+
+//STEP 11
+
+//STEP 12
+
+//STEP 13
+
+//STEP 14
+
+//STEP 15
+
+//STEP 16
+
+//STEP 17
